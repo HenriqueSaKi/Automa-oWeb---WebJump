662c73d215e151a0cde44d02f9961603edcca093
Henrique-Kisaki <satoshikisaki@hotmail.com>
2020-07-31 20:55:08 -0300

    comentários adicionados ao programa

--- QA-WebJump.py
+++ QA-WebJump.py
@@ -14,9 +14,11 @@ driver.maximize_window()
 
 #Clica e verifica existência dos botões do Box Buttons
 class ClickButtons ():
+    #Função de inicialização da classe
     def __init__(self):
         pass
 
+    #Procura e clica nos botões selecionados
     def ClicaBotao (self):
         botaoOne = driver.find_element_by_xpath("//div[@id='panel_body_one']//button[contains(text(),'One')]")
         botaoOne.click()
@@ -25,54 +27,26 @@ class ClickButtons ():
         botaoFour = driver.find_element_by_xpath("//div[@id='panel_body_one']//button[contains(text(),'Four')]")
         botaoFour.click()
 
-    def Verifica (self):
+    #Verifica a existência dos botões
+    def VerificaBox (self):
         pass
 
+    #Executa todas as tarefas da classe
     def run (self):
         self.ClicaBotao()  
 
-class LastScene ():
-    def __init__(self):
-        pass
-
-    def nameField(self): #preencher o campo "YourFirstName" com um texto qualquer
-        nameField = driver.find_element_by_xpath("//div[@class='form-group']//input[@placeholder='YourFirstName']")
-        nameField.send_keys("Henrique")
-
-    def ClickBotOne (self): #Clique no botão "One"
-        botaoOne = driver.find_element_by_xpath("//div[@id='panel_body_one']//button[contains(text(),'One')]")
-        botaoOne.click()
-
-    def CheckBox(self): #cheque a opção "OptionThree"
-        checkOption = driver.find_element_by_xpath("//div[@class='checkbox']//input[@id='opt_three']")
-        checkOption.click()
-
-    def SelectFromList (self): #selecione a opção "ExampleTwo" dentro da select box
-        FromListOption = driver.find_element_by_xpath("//select[@id='select_box']//option[@value='option_two']")
-        FromListOption.click()
-
-    def VisibilityOfSeleniumImg (self): #valide a presença da imagem do logo do "Selenium Webdriver"
-        try:
-            ImgPresence = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//div[@id='panel_body_three']//img[@alt='selenium']")))
-            print ("It's visible!")
-        except: 
-            print ("Sorry, We couldn't find anything!")
-    
-    def run (self):
-        self.nameField()
-        self.ClickBotOne()      
-        self.CheckBox()
-        self.SelectFromList()
-        self.VisibilityOfSeleniumImg()  
-
+#Clica e verifica existência dos botões no iframe
 class ClickIFrameButtons ():
+    #Função de inicialização da classe
     def __init__(self):
         pass
 
+    #Acessa o iframe, para realizar modificações nele
     def iFrameAccess (self):
         iframe = driver.find_element_by_xpath("//iframe[@src='buttons.html']")
         driver.switch_to_frame(iframe)
-
+    
+    #Procura e clica nos botões selecionados
     def ClicaBotao (self):
         botaoOneiframe = driver.find_element_by_xpath("//div[@class='col-sm-12']//button[contains(text(),'One')]")
         botaoOneiframe.click()
@@ -82,16 +56,62 @@ class ClickIFrameButtons ():
         botaoFouriframe.click()
         driver.switch_to_default_content()
 
-    def Verifica (self):
+    #Verifica a existência dos botões no iframe
+    def VerificaIframe (self):
         pass    
 
+    #Executa todas as tarefas da classe
     def run (self):
         self.iFrameAccess()
         self.ClicaBotao()
 
-#CB = ClickButtons() #CLASSE DO CENÁRIO 1 
+
+class LastScene ():
+    #Função de inicialização da classe
+    def __init__(self):
+        pass
+
+    #Preencher o campo "YourFirstName" com um texto qualquer
+    def nameField(self): 
+        nameField = driver.find_element_by_xpath("//div[@class='form-group']//input[@placeholder='YourFirstName']")
+        nameField.send_keys("Henrique")
+
+    #Clique no botão "One"
+    def ClickBotOne (self): 
+        botaoOne = driver.find_element_by_xpath("//div[@id='panel_body_one']//button[contains(text(),'One')]")
+        botaoOne.click()
+
+    #Cheque a opção "OptionThree"
+    def CheckBox(self): 
+        checkOption = driver.find_element_by_xpath("//div[@class='checkbox']//input[@id='opt_three']")
+        checkOption.click()
+
+    #Selecione a opção "ExampleTwo" dentro da select box
+    def SelectFromList (self): 
+        FromListOption = driver.find_element_by_xpath("//select[@id='select_box']//option[@value='option_two']")
+        FromListOption.click()
+
+    #Verifica a existência da imagem com o logo do "selenium"
+    def VisibilityOfSeleniumImg (self): #valide a presença da imagem do logo do "Selenium Webdriver"
+        try:
+            ImgPresence = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//div[@id='panel_body_three']//img[@alt='selenium']")))
+            print ("It's visible!")
+        except: 
+            print ("Sorry, We couldn't find anything!")
+    
+    #Executa todas as tarefas da classe
+    def run (self):
+        self.nameField()
+        self.ClickBotOne()      
+        self.CheckBox()
+        self.SelectFromList()
+        self.VisibilityOfSeleniumImg()  
+
+#Para a realização dos testes, deve-se descomentar a classe e seu respectivo "executa cenário"
+
+CB = ClickButtons() #CLASSE DO CENÁRIO 1 
 #CIF = ClickIFrameButtons() #CLASSE DO CENÁRIO 2
-LS = LastScene() #CLASSE DO CENÁRIO 3
-#CB.run() #EXERCUTA CENÁRIO 1
+#LS = LastScene() #CLASSE DO CENÁRIO 3
+CB.run() #EXERCUTA CENÁRIO 1
 #CIF.run() #EXECUTA CENÁRIO 2
-LS.run() #EXECUTA CENÁRIO 3
+#LS.run() #EXECUTA CENÁRIO 3
